db203d4e4229d63b841aeeb0ea7a1fae006fd738
BlueNinja1 <lopzac981@gmail.com>
2024-10-20 22:47:01 -0400

    Assignment Complete.

--- main.py
+++ main.py
@@ -5,22 +5,28 @@
 # Get input and convert to correct data type for testScore and classRank
 
 # Test using admission requirements and print Accept or Reject
+
+print("Please Enter this student's test score...")
+testScore = int(input())
+print("Now please Enter this student's class rank...")
+classRank = int(input())
+
 if testScore >= 90:
   if classRank >= 25:
-    print("Accept")
+    print("Admission Accepted...")
   else:
-    print("Reject")
+    print("Admission Rejected...")
 else:
   if testScore >= 80:
     if classRank >= 50:
-      print("Accept")
+      print("Admission Accepted...")
     else:
-      print("Reject")
+      print("Admission Rejected...")
   else:
     if testScore >= 70:
       if classRank >= 75:
-        print("Accept")
+        print("Admission Accepted...")
       else:
-        print("Reject")
+        print("Admission Rejected...")
     else:
-      print("Reject")
+      print("Admission Rejected...")
